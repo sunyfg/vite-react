317190d8d274db81b898a553b29c08bd2561e909
孙验沣 <sunyanfeng@sunyanfengdeMacBook-Pro.local>
2022-03-10 18:16:28 +0800

    commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+console.log(import.meta.env);
+const x = import.meta.env.VITE_AAA
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
