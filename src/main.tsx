9c572e845dfce2631efa6a1d0423c20e0d73a2a1
孙验沣 <sunyanfeng@sunyanfengdeMacBook-Pro.local>
2022-03-11 10:17:34 +0800

    add prettierrc

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-console.log(import.meta.env);
-const x = import.meta.env.VITE_AAA
+console.log(import.meta.env)
+// const x = import.meta.env.VITE_AAA
 
 ReactDOM.render(
   <React.StrictMode>
